e4a2d89c329e2c1706591fc00be21c91d68d480c
Rakeen Rouf <36940292+rmr327@users.noreply.github.com>
2023-12-09 05:30:13 +0000

    u

--- word_cloud.py
+++ word_cloud.py
@@ -1,15 +1,21 @@
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import pandas as pd
+from nltk.corpus import stopwords
 
 
 if __name__ == '__main__':
     print(21)
-    df_probabilities = pd.read_csv('tfidf_model_probabilities.csv')
+    df_probabilities = pd.read_csv('synthetic_tfidf_model_probabilities.csv')
 
-    # Create a dictionary of words and their probabilities
+    # Create a set of English stop words
+    stop_words = set(stopwords.words('english'))
+
+    # Create a dictionary of words and their probabilities, excluding stop words
     word_probs = {word: prob for word, prob in zip(df_probabilities['Word'], 
-                                                df_probabilities['Positive'])}
+                                                   df_probabilities['Positive']) 
+                                                   if word not in stop_words}
+
 
     # Create a WordCloud object
     wordcloud = WordCloud(width=800, height=400, background_color='white')
@@ -22,4 +28,4 @@ if __name__ == '__main__':
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
     # save the image
-    plt.savefig('real_data_word_cloud_positive.png')
+    plt.savefig('synthetic_word_cloud_positive.png')
